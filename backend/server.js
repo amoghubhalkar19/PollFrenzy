c488a6763c8b9e76b1b63d9c98dd1a3832a7a1ac
amoghubhalkar19 <118107744+amoghubhalkar19@users.noreply.github.com>
2023-07-31 22:49:38 +0530

    latest commit

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,61 @@
 const express = require('express');
-const path= require('path');
-const fs= require('fs');
+const path = require('path');
+const fs = require('fs');
+const exp = require('constants');
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
-app.listen(port, ()=>{
-    console.log(`listening on port ${port}`)
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
 
 
-app.post('/create', (req, res)=>{
-    const data=req.body.data;
+app.post('/create', (req, res) => {
     console.log("post req accepted");
+    const data = req.body;
+    console.log(data);
 
-    
-    fs.writeFileSync('./data.json', JSON.stringify(data), err =>{
-        if(err){
+    var baseData = []
+    const jsonString = fs.readFileSync('./data.json', { encoding: 'utf-8' });
+    console.log(jsonString);
+    jsonObj = JSON.parse(jsonString);
+    baseData.push(jsonObj);
+    baseData.push(data);
+    console.log(baseData);
+    fs.writeFileSync('./data.json', JSON.stringify(baseData), err => {
+        if (err) {
             console.log("something went wrong");
         }
-        else{
+        else {
             console.log("submitted successfully");
         }
     })
 
 })
+
+
+app.post('/vote', (req, res) => {
+    console.log("vote req accepted");
+    const voteid = req.body;
+    let id=voteid.id;
+    
+    console.log(id);
+})
+
+
+
+//search id
+//make a compo to vote now
+//return data to compo
+//keep record of no. of votes to each option
+//return data to monitor section--live
+//send email of id thru nodemailer
+
